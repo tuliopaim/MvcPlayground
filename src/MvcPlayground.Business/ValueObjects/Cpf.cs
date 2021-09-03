17179a5e5370e286acd826e685d16bd5598774e1
Túlio Paim <tutpaim@gmail.com>
2021-09-03 12:30:43 -0300

    Simplifica validação de CPF

--- src/MvcPlayground.Business/ValueObjects/Cpf.cs
+++ src/MvcPlayground.Business/ValueObjects/Cpf.cs
@@ -26,41 +26,21 @@ namespace MvcPlayground.Business.ValueObjects
 
             var cpfSemDigito = _cpf[..9];
 
-            var primeiroDigito = CalcularPrimeiroDigito(cpfSemDigito);
-            var segundoDigito = CalcularSegundoDigito(cpfSemDigito, primeiroDigito);
+            var primeiroDigito = CalcularDigito(cpfSemDigito);
+            var segundoDigito = CalcularDigito(cpfSemDigito + primeiroDigito);
             
             return _cpf.EndsWith($"{primeiroDigito}{segundoDigito}");
         }
 
-        private static int CalcularPrimeiroDigito(string cpfSemDigito)
+        private static int CalcularDigito(string cpfTemp)
         {
-            var multiplicador1 = new[] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-
-            var soma = 0;
-
-            for (var i = 0; i < 9; i++)
-                soma += int.Parse(cpfSemDigito[i].ToString()) * multiplicador1[i];
+            var soma = cpfTemp.Select((digito, i) 
+                => int.Parse(digito.ToString()) * cpfTemp.Length - i).Sum();
 
             var resto = soma % 11;
             var digito = resto < 2 ? 0 : 11 - resto;
 
             return digito;
         }
-        
-        private static int CalcularSegundoDigito(string cpfSemDigito, int primeiroDigito)
-        {
-            var multiplicador2 = new[] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-            var cpfTemporario = cpfSemDigito + primeiroDigito;
-
-            var soma = 0;
-            for (var i = 0; i < 10; i++)
-                soma += int.Parse(cpfTemporario[i].ToString()) * multiplicador2[i];
-
-            var resto = soma % 11;
-            var digito = resto < 2 ? 0 : 11 - resto;
-
-            return digito;
-        }
-
     }
 }
